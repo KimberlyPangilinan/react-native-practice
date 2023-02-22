3473b2bdee4058a85e69309d983a8dd78c2b682f
kim <kimberlypangilinan2001@gmail.com>
2023-02-22 19:57:04 +0800

    setup practice

---
+++ first-react-native-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Sa wakas napagana rin kita!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ first-react-native-app/readme.md
@@ -0,0 +1,8 @@
+> https://docs.expo.dev/tutorial/create-your-first-app/
+
+1. create and run your project with expo cli
+   `npm install --global expo-cli`
+2. `npx create-expo-app`
+3. change the assets of the proj optional
+4. install dependencies that will help to run the project on the web: `npx expo install react-dom react-native-web @expo/webpack-config`
+5. `npx expo start` or `expo start`
